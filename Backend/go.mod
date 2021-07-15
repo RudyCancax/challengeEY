47e64501d137bedb449bbe0285dc823def1c1616
RudyCancax <rudycancax@gmail.com>
2021-07-14 21:49:36 -0600

    Backend GO - MongoDB

---
+++ Backend/go.mod
@@ -0,0 +1,13 @@
+module backend
+
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/go-playground/validator/v10 v10.7.0 // indirect
+	github.com/heroku/x v0.0.31 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	go.mongodb.org/mongo-driver v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+)
--- Backend/main.go
+++ Backend/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+    "os"
+
+    middleware "backend/middleware"
+    routes "backend/routes"
+
+    "github.com/gin-gonic/gin"
+    _ "github.com/heroku/x/hmetrics/onload"
+)
 
 func main() {
-	fmt.Println("Conexión a MongoDB")
+    port := os.Getenv("PORT")
+
+    if port == "" {
+        port = "8000"
+    }
+
+    router := gin.New()
+    router.Use(gin.Logger())
+    routes.UserRoutes(router)
+
+    router.Use(middleware.Authentication())
+
+    // API-2
+    router.GET("/api-1", func(c *gin.Context) {
+
+        c.JSON(200, gin.H{"success": "Access granted for api-1"})
+
+    })
+
+    // API-1
+    router.GET("/api-2", func(c *gin.Context) {
+        c.JSON(200, gin.H{"success": "Access granted for api-2"})
+    })
+
+    router.Run(":" + port)
 }
