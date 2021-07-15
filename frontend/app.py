1507ec2b2491ef3973626ce20ddccfe3f3b90d66
RudyCancax <rudycancax@gmail.com>
2021-07-15 12:00:57 -0600

    Diseño y desarrollo de Pantalla Login (en base al diseño)

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def principal():
     return "<h1>HOLAAAA MUNDO FLASK</h1>"
+
+@app.route('/login')
+def login():
+    return render_template(
+        "index.html"
+    ) 
+
+@app.route('/home/<nombre>')
+def loged(nombre):
+    return "<h2>HOLAAAAA JOVEN, {}</h2>".format(nombre)
 
 if __name__ == '__main__':
     app.run
